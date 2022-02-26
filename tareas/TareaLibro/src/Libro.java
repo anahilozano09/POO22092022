15def022a407a75ff6cba6d3034f95bd39428eaa
anahilozano09 <anahilozano319@aragon.unam.mx>
2022-02-26 13:47:22 -0600

    iniciamos el archivo Libro.java

--- tareas/TareaLibro/src/Libro.java
+++ tareas/TareaLibro/src/Libro.java
@@ -16,4 +16,57 @@ public class Libro {
     private Color color;
     private String empastado;
     private String papel;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String editorial, Color color, String empastado, String papel) {
+        this.titulo = titulo;
+        this.editorial = editorial;
+        this.color = color;
+        this.empastado = empastado;
+        this.papel = papel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getEmpastado() {
+        return empastado;
+    }
+
+    public void setEmpastado(String empastado) {
+        this.empastado = empastado;
+    }
+
+    public String getPapel() {
+        return papel;
+    }
+
+    public void setPapel(String papel) {
+        this.papel = papel;
+    }
+    
+    
 }
