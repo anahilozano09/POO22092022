c0c55f5e3c8de11562ce60f2acf1c49b5caa51d5
anahilozano09 <anahilozano319@aragon.unam.mx>
2022-02-26 13:46:49 -0600

    iniciamos el archivo Libro.java

---
+++ tareas/TareaLibro/src/Libro.java
@@ -0,0 +1,19 @@
+
+import java.awt.Color;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author anahi
+ */
+public class Libro {
+    private String titulo;
+    private String editorial;
+    private Color color;
+    private String empastado;
+    private String papel;
+}
