460f5c5340b39825b56a4c15678602cba628e5c0
anahilozano09 <anahilozano319@aragon.unam.mx>
2022-05-06 20:59:16 -0500

    Commit de la clase del 05 de mayo

--- clases/JavaBasico2209Parte2/src/ico/fes/awt/Ventana.java
+++ clases/JavaBasico2209Parte2/src/ico/fes/awt/Ventana.java
@@ -4,15 +4,25 @@
  */
 package ico.fes.awt;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.HeadlessException;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  *
  * @author anahi
  */
-public class Ventana extends Frame {
+public class Ventana extends Frame implements MouseListener {
     private String titulo;
+    private Button boton1;
+    private FlowLayout layout;
+    private Label etiqueta;
+    private TextField cuadroTexto;
 
     public Ventana() throws HeadlessException {
     }
@@ -20,7 +30,19 @@ public class Ventana extends Frame {
     public Ventana(String titulo) throws HeadlessException {
         super(titulo);
         this.titulo = titulo;
-        this.setSize(400,600);
+        this.setSize(350,220);
+        layout = new FlowLayout(FlowLayout.LEFT);
+        this.setLayout(layout);
+        boton1 = new Button("Presioname");
+        cuadroTexto = new TextField(15);
+        etiqueta = new Label("Texto inicial");
+        
+        this.add(cuadroTexto);
+        this.add(boton1);
+        this.add(etiqueta);
+        
+        this.boton1.addMouseListener(this);
+        
         this.setVisible(true);
     }
 
@@ -35,6 +57,31 @@ public class Ventana extends Frame {
     @Override
     public String toString() {
         return "Ventana{" + "titulo=" + titulo + '}';
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        etiqueta.setText("Hola "+cuadroTexto.getText());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        
     }
     
     
--- clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
+++ clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -5,6 +5,7 @@
 package javabasico2209parte2;
 
 import ico.fes.awt.Ventana;
+import ico.fes.awt.VentanaVersion2;
 
 /**
  *
@@ -12,6 +13,7 @@ import ico.fes.awt.Ventana;
  */
 public class JavaGraficoMain {
     public static void main(String[] args) {
-        Ventana ventana = new Ventana("Hola AWT ICO FES Aragon");
+        //Ventana ventana = new Ventana("Hola AWT ICO FES Aragon");
+        VentanaVersion2 ventana = new VentanaVersion2();
     }
 }
